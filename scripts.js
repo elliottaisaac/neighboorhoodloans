6fe8e7791f86e7765c4d23eb60d74781b63f476a
Elliott Isaac <eisaac22@gmail.com>
2020-09-28 17:03:17 -0400

    js comment

--- scripts.js
+++ scripts.js
@@ -55,7 +55,7 @@ window.addEventListener("scroll", () => {
 
 
  
-
+//INTERACTIVE STEPS ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 let stepText = [
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
     "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet.",
