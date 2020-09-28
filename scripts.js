878859e668fcc958a7f0bccf105dcdc7d0a1a9b7
Elliott Isaac <eisaac22@gmail.com>
2020-09-28 17:05:44 -0400

    CSS cleanup

--- scripts.js
+++ scripts.js
@@ -53,8 +53,6 @@ window.addEventListener("scroll", () => {
     document.querySelectorAll(".check").forEach(c =>{ c.style.transform = `scale(${scale})`; });
 });
 
-
- 
 //INTERACTIVE STEPS ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 let stepText = [
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
